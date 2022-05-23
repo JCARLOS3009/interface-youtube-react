a918eca93e9046710c96bb4c458e33d28cc72dd6
JOSE CARLOS BARRETO FILHO <85513751+JCARLOS3009@users.noreply.github.com>
2022-05-23 17:30:55 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core';
+
+import Home from './Home';
+
+const useStyles = makeStyles({
+  root: {},
+});
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    spacing: 4,
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f44336',
+      },
+      secondary: {
+        main: '#3EA6FF',
+      },
+      background: {
+        default: darkMode ? '#232323' : '#FFF',
+        dark: darkMode ? '#181818' : '#f4f6f8',
+        paper: darkMode ? '#232323' : '#FFF',
+      },
+    },
+  });
+
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home darkMode={darkMode} setDarkMode={setDarkMode} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
